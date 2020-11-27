9f214a5dbf971f27db7ac2a3e2c245e477f8659a
Alexis Guzman <alexis12989x@gmail.com>
2020-11-27 11:41:15 -0600

    Import MoviesContextProvider component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import MoviesContextProvider from './components/contexts/MoviesContext';
+
 import App from './App';
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <MoviesContextProvider>
+            <App />
+        </MoviesContextProvider>
     </React.StrictMode>,
     document.getElementById('root')
 );
