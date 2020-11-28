1a4e55e863f22a74a22d7b7f7297b8fd9534206d
Alexis Guzman <alexis12989x@gmail.com>
2020-11-28 11:58:47 -0600

    Set up Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,23 @@
+import { useContext } from 'react';
+
+import { MoviesContext } from './contexts/MoviesContext';
+
 const Header = () => {
+    const { search, setSearch, handleSubmit } = useContext(MoviesContext);
+
     return (
-        <div>
-            <p>Header Component</p>
-        </div>
+        <header className='header'>
+            <h1 className='header__title'>Movies App</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    placeholder='Search...'
+                    className='header__search'
+                    type='search'
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                />
+            </form>
+        </header>
     );
 };
 
