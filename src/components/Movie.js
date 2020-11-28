334b3197011a042d4a9d5f343dd7923f3e79fbab
Alexis Guzman <alexis12989x@gmail.com>
2020-11-28 11:58:31 -0600

    Set up Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,8 +1,36 @@
-const Movie = () => {
+import { IMG_API } from '../api';
+
+import NotFoundImg from '../img/not-found.jpg';
+
+const Movie = ({ poster_path, title, vote_average, overview }) => {
+    const setVoteClass = (vote) => {
+        if (vote >= 8) return 'green';
+        else if (vote >= 6) return 'orange';
+        else return 'red';
+    };
+
     return (
-        <div>
-            <p>Movie Component</p>
-        </div>
+        <section className='movie'>
+            <img
+                src={poster_path ? IMG_API + poster_path : NotFoundImg}
+                alt={title}
+            />
+            <div className='movie__info'>
+                <h3>{title}</h3>
+                <span
+                    className={`movie__average movie__average-${setVoteClass(
+                        vote_average
+                    )}`}
+                >
+                    {vote_average}
+                </span>
+            </div>
+
+            <div className='movie__overview'>
+                <h4>Overview:</h4>
+                <p>{overview}</p>
+            </div>
+        </section>
     );
 };
 
