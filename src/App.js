2b2bacfd8dbe504c9cc7b62ff7da54f171616030
Alexis Guzman <alexis12989x@gmail.com>
2020-11-28 11:58:59 -0600

    Set up App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import { useContext } from 'react';
+
+import { MoviesContext } from './components/contexts/MoviesContext';
+
+import Movie from './components/Movie';
+import Header from './components/Header';
 
 const App = () => {
+    const { movies } = useContext(MoviesContext);
+
+    const mapMovies = () =>
+        movies.map((movie) => <Movie key={movie.id} {...movie} />);
+
     return (
         <div className='app'>
-            <p>App Component</p>
+            <Header />
+            <main className='movies'>{movies && mapMovies()}</main>
         </div>
     );
 };
 
+// @import './layout/movies';
 export default App;
