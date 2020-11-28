c5b4ae89dbaaae150cf7fca21cae386382620314
Alexis Guzman <alexis12989x@gmail.com>
2020-11-28 11:58:10 -0600

    Set up MoviesContext for get and set movies

--- src/components/contexts/MoviesContext.js
+++ src/components/contexts/MoviesContext.js
@@ -1,12 +1,34 @@
 import { useState, useEffect, createContext } from 'react';
 
+import { FEATURED_API, SEARCH_API } from '../../api';
+
 export const MoviesContext = createContext();
 
 const MoviesContextProvider = ({ children }) => {
-    const [movies, setMovies] = useState([]);
+    const [movies, setMovies] = useState(null);
+    const [search, setSearch] = useState('');
+
+    useEffect(() => {
+        getMovies(FEATURED_API);
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (search) getMovies(SEARCH_API + search);
+    };
+
+    const getMovies = async (API) => {
+        const res = await fetch(API);
+        const movies = await res.json();
+
+        setMovies(movies.results);
+    };
 
     return (
-        <MoviesContext.Provider value={{ movies }}>
+        <MoviesContext.Provider
+            value={{ movies, search, setSearch, handleSubmit }}
+        >
             {children}
         </MoviesContext.Provider>
     );
